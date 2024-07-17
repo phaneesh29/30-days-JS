660ad42833830c31b0c157a5d126674d2dbe960b
sreephaneesha <=>
2024-07-17 08:41:59 +0530

    day02

---
+++ Day-02/01_operators.js
@@ -0,0 +1,55 @@
+// Activity-1
+
+let number1 = 34
+let number2 = 56
+console.log(`${number1} + ${number2} = ${number1 + number2}`);
+console.log(`${number1} - ${number2} = ${number1 - number2}`);
+console.log(`${number1} X ${number2} = ${number1 * number2}`);
+console.log(`${number1} / ${number2} = ${number1 / number2}`);
+console.log(`${number2} % ${number1} = ${number2 % number1}`);
+
+console.log("---------------------------------------------------------------------------------------------------------------");
+console.log("---------------------------------------------------------------------------------------------------------------");
+
+//Activity-2
+
+let anotherNumber = 23
+number1 = 7
+anotherNumber += number1
+console.log(`anotherNumber = ${anotherNumber}`);
+
+anotherNumber -= number1
+console.log(`anotherNumber = ${anotherNumber}`);
+
+console.log("---------------------------------------------------------------------------------------------------------------");
+console.log("---------------------------------------------------------------------------------------------------------------");
+
+// Activity-3
+
+console.log(`Is ${number1} greater than ${number2}? ${number1 > number2}`);
+console.log(`Is ${number1} lesser than ${number2}? ${number1 < number2}`);
+
+
+console.log(`Is ${number1} greater than or equal to ${number2}? ${number1 >= number2}`);
+console.log(`Is ${number1} lesser than or equal to ${number2}? ${number1 <= number2}`);
+
+
+console.log(`Is ${number1}  equal to ${number2}? ${number1 == number2}`);
+console.log(`Is ${number1} strictly equal to ${number2}? ${number1 === number2}`);
+
+
+console.log("---------------------------------------------------------------------------------------------------------------");
+console.log("---------------------------------------------------------------------------------------------------------------");
+
+// Activity-4
+console.log("7 > 5 && 6 > 7  ", 7 > 5 && 6 > 7);
+console.log("7 > 5 || 6 > 7  ", 7 > 5 || 6 > 7);
+console.log("! (6 > 7)  ", !(6 > 7));
+
+console.log("---------------------------------------------------------------------------------------------------------------");
+console.log("---------------------------------------------------------------------------------------------------------------");
+
+let checkNum = -44
+let isPositive = checkNum>0 ? "Positive" : "Negative"
+console.log(`${checkNum} is ${isPositive}`);
+
